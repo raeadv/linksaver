28c27ba8adbe0b09bde9834cb93d49e5681de860
raeadv <aeraeneldis@hotmail.com>
2026-03-08 19:26:17 +0700

    add conditional render for auth, add links data display

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -3,17 +3,30 @@ import LinkForm from '@/components/LinkForm.vue';
 import TagsForm from '@/components/TagsForm.vue';
 import UserInfo from '@/components/UserInfo.vue';
 import { useAuthStore } from '@/store/authStore';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+import Login from './auth/Login.vue';
+import LinksData from '@/components/LinksData.vue';
 
 const store = useAuthStore()
-const user = computed(() => store.userdata)
+const linksDataRef = ref()
 
+const handleRefreshLinkData = () => {
+    linksDataRef.value.fetchLinks()
+}
 </script>
 
 <template>
-    <UserInfo />
-    <div class="mt-2 my-2 w-full flex flex-row space-x-2">
-        <TagsForm />
-        <LinkForm />
+    <div v-if="store.isAuthenticated" class="w-full">
+        <UserInfo />
+        <div class="mt-2 my-2 w-full flex flex-row space-x-2">
+            <TagsForm />
+            <LinkForm @link-created="handleRefreshLinkData" />
+        </div>
+        <div class="mt-2">
+            <LinksData ref="linksDataRef" />
+        </div>
+    </div>
+    <div v-else class="w-full flex items-center justify-center">
+        <Login />
     </div>
 </template>
