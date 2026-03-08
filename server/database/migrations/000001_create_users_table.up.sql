eba4e52d092fabe9dc8a9f383e2f72aff2a11862
raeadv <aeraeneldis@hotmail.com>
2026-03-08 09:33:16 +0700

    add index key

--- server/database/migrations/000001_create_users_table.up.sql
+++ server/database/migrations/000001_create_users_table.up.sql
@@ -1,10 +1,14 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS users(
-   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+   id UUID PRIMARY KEY DEFAULT uuidv7(),
    name VARCHAR (150) NOT NULL UNIQUE,
    username VARCHAR (50) UNIQUE NOT NULL,
    password TEXT NOT NULL,
    email VARCHAR (300) UNIQUE NOT NULL,
    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
+-- add indexes
+CREATE INDEX user_id_index ON users (id);
+CREATE INDEX user_username_index ON users (username);
--- server/database/migrations/000002_create_links_table.up.sql
+++ server/database/migrations/000002_create_links_table.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS tags (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     name VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS links (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     link TEXT NOT NULL,
     name VARCHAR(150) NOT NULL,
@@ -17,3 +17,11 @@ CREATE TABLE IF NOT EXISTS link_tags (
     tag_id  UUID NOT NULL REFERENCES tags(id)  ON DELETE CASCADE,
     PRIMARY KEY (link_id, tag_id)
 );
+
+-- add indexes
+CREATE INDEX tag_id_index ON tags (id);
+CREATE INDEX tag_userid_index ON tags (user_id);
+CREATE INDEX link_id_index ON links (id);
+CREATE INDEX link_userid_index ON links (user_id);
+CREATE INDEX linktag_link_id_index ON link_tags (link_id);
+CREATE INDEX linktag_tagid_index ON link_tags (tag_id);
