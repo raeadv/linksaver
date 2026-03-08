ff784898100d6646b5d8b13484b781361acb5996
raeadv <aeraeneldis@hotmail.com>
2026-03-08 19:26:32 +0700

    add settings page on route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,12 +5,14 @@ import HomePage from '@/pages/Home.vue'
 import Register from '@/pages/auth/Register.vue'
 import Login from '@/pages/auth/Login.vue'
 import { createDiscreteApi } from 'naive-ui'
+import SettingsPage from '@/pages/SettingsPage.vue'
 
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: HomePage },
+    { path: '/settings', component: SettingsPage },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
   ],
