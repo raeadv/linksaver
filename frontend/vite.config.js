94b6548eafac7305c512a234b14134a3273ac764
raeadv <aeraeneldis@hotmail.com>
2026-03-10 12:46:27 +0700

    adjust origin and credential header on js for secure cookie

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,46 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  envDir: path.resolve(__dirname, "../"),
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(mode => {
+  const env = loadEnv(mode, path.resolve(__dirname, '../'), '')
+  const baseUrl = env.VITE_BASE_URL
+
+  const [host, port] = parseBaseUrl(baseUrl)
+
+  return {
+    envDir: path.resolve(__dirname, "../"),
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  build: {
-    emptyOutDir: true,
-    outDir: path.resolve(__dirname, './../compiled/html'),
-    minify: true,
+    build: {
+      emptyOutDir: true,
+      outDir: path.resolve(__dirname, './../compiled/html'),
+      minify: true,
+    },
+    server: {
+      host: host,
+      port: port
+    }
   }
 })
+
+
+const parseBaseUrl = base_url => {
+
+  const parts = String(base_url).split(':')
+  const host = String(parts[1]).replace("//", "")
+
+  return [host, parts[2]]
+}
--- server/main.go
+++ server/main.go
@@ -41,8 +41,10 @@ func main() {
 
 	g := gin.Default()
 
+	frontendUrl := os.Getenv("BASE_URL")
+
 	g.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{frontendUrl},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Access-Control-Allow-Origin"},
 		AllowCredentials: true,
