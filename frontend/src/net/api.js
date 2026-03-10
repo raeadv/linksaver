5badb274d9a4916c7beaca2a0b06ac798ba372a7
raeadv <aeraeneldis@hotmail.com>
2026-03-10 12:46:44 +0700

    add withCredential

--- frontend/src/net/api.js
+++ frontend/src/net/api.js
@@ -12,6 +12,7 @@ const getNetState = () => netState();
 const baseUrl = import.meta.env.VITE_API_URL ?? 'http://localhost:8080';
 const api = axios.create({ baseURL: baseUrl })
 api.showErrMessage = true
+api.defaults.withCredentials = true
 
 let isRefreshing = false;
 let queue = [];
